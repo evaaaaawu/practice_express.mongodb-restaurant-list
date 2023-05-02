baeab856a9ef39506046e9dcc37c91db95bb95e9
evaaaaawu <yiju.wu1@gmail.com>
2023-05-02 11:56:38 +0800

    feat: add search routes

--- app.js
+++ app.js
@@ -25,6 +25,14 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
   res.render('show', { restaurant: restaurant })
 })
 
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
