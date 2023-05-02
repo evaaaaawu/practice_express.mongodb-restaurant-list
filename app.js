bb2460bbdfa2a6db336cb94eb3811b714491286a
evaaaaawu <yiju.wu1@gmail.com>
2023-05-02 16:59:20 +0800

    feat: connect mongodb and add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 // require packages used in the project
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose') // 載入 mongoose
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
 const port = 3000
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 const restaurantList = require('./restaurant.json')
 
