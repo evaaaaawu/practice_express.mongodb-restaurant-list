96a02ef025f0cdee3e7aef5bc0864eb2a3762a2f
evaaaaawu <yiju.wu1@gmail.com>
2023-05-02 11:29:22 +0800

    feat: add handlebars, static files and use json format data

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 // require packages used in the project
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+const restaurantList = require('./restaurant.json')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
 // routes setting
 app.get('/', (req, res) => {
-  res.send('This is my restaurant list built with Express')
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {  
+  const restaurant = restaurantList.results.find(
+    restaurant => restaurant.id.toString() === req.params.restaurant_id
+  ) 
+  res.render('show', { restaurant: restaurant })
 })
 
 // start and listen on the Express server
