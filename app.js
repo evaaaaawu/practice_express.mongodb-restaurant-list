b0e98434459edfdce267ce30d6cc4a125325f31b
evaaaaawu <yiju.wu1@gmail.com>
2023-05-03 11:57:33 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose') // 載入 mongoose
+const bodyParser = require('body-parser') // 引用 body-parser
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -25,6 +26,7 @@ db.once('open', () => {
 })
 
 const restaurantList = require('./restaurant.json')
+const Restaurant = require('./models/restaurant')
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -32,10 +34,15 @@ app.set('view engine', 'handlebars')
 
 // setting static files
 app.use(express.static('public'))
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // routes setting
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {  
@@ -51,6 +58,16 @@ app.get('/search', (req, res) => {
     return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
   })
   res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post("/restaurants", (req, res) => {
+  Restaurant.create(req.body)
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err))
 })
 
 // start and listen on the Express server
