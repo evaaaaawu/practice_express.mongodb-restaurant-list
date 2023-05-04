afcc30699d762aac101fae6ce1f3a226fc15cf0c
evaaaaawu <yiju.wu1@gmail.com>
2023-05-04 16:42:16 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -14,5 +14,10 @@ const restaurants = require('./modules/restaurants')
 // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組 
 router.use('/restaurants', restaurants)
 
+// 引入 users 模組程式碼
+const users = require('./modules/users')
+// 將網址結構符合 /users 字串開頭的 request 導向 users 模組 
+router.use('/users', users)
+
 // 匯出路由器
 module.exports = router
