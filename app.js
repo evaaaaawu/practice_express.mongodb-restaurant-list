be160f4ccfed35deb64e23b06769160607c35d66
evaaaaawu <yiju.wu1@gmail.com>
2023-05-05 09:31:04 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // require packages used in the project
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const bodyParser = require('body-parser') // 引用 body-parser
 const methodOverride = require('method-override')
 
@@ -20,9 +21,12 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-// setting static files
 app.use(express.static('public'))
-// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(session({
+  secret: 'ThisIsMySecret', //session 用來驗證 session id 的字串
+  resave: false, //當設定為 true 時，會在每一次與使用者互動後，強制把 session 更新到 session store 裡。
+  saveUninitialized: true //強制將未初始化的 session 存回 session store。未初始化表示這個 session 是新的而且沒有被修改過，例如未登入的使用者的 session。
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes) // 將 request 導入路由器
